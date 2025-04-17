66ebf8c934093205aad4faa78fe1790e09583c7b
Andi T. Sylveon Deer <waternewgatchi@gmail.com>
2025-04-17 09:25:50 -0400

    mod: remove wawaworld from rdm4001 repo it should now be at https://github.com/entropy-interactive-software/wawaworld

--- wawaworld/gstate.cpp
+++
@@ -1,9 +0,0 @@
-#include "gstate.hpp"
-
-#include "game.hpp"
-#include "state.hpp"
-namespace ww {
-WWGameState::WWGameState(rdm::Game* game) : rdm::GameState(game) {
-  // stateMusic[MainMenu] = "dat5/mus/main_menu.ogg";
-}
-}  // namespace ww
--- wawaworld/gstate.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "game.hpp"
-#include "state.hpp"
-namespace ww {
-class WWGameState : public rdm::GameState {
- public:
-  WWGameState(rdm::Game* game);
-};
-}  // namespace ww
